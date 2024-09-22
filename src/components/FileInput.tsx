162119ec3f659eaf0203e9d37ae06839a57fb5fe
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-09-22 21:46:47 +0400

    make something i do not know how to express -context thing-

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,13 +1,16 @@
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useContentFile } from "@/lib/store"
 export function InputFile() {
+  const { setContent } = useContentFile()
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0]
     if (!file) return
     const reader = new FileReader()
     reader.onload = function(e) {
       const content = e.target?.result as string
+      setContent(content)
     }
     reader.readAsText(file)
   }
