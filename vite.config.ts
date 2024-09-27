d3daebe85ca82e907e01869ac78a9ac0e8888f37
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-09-27 21:31:56 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/eduwave_to_csv/",
   plugins: [
     compression(),
     react(),
